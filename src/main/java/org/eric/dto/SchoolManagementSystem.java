267f05d7f9ed4aaffd009a35d1b0a5730d450aaa
Eric Tiganasu <6223598@edu.vaniercollege.qc.ca>
2023-11-26 11:28:35 -0500

    All the classes have been documented and empty methods with documentation have been added to the SchoolManagementSystem.java class.

--- src/main/java/org/eric/dto/SchoolManagementSystem.java
+++ src/main/java/org/eric/dto/SchoolManagementSystem.java
@@ -5,4 +5,99 @@ package org.eric.dto;
  * The school management system class
  */
 public class SchoolManagementSystem {
+
+    /**
+     * Finds a department based on its id
+     * @return specified department
+     */
+    public Department findDepartment() {
+
+    }
+
+    /**
+     * Displays the teachers
+     */
+    public void printTeachers() {
+
+    }
+
+    /**
+     * Modifies the teacher of a course
+     */
+    public void modifyCourseTeacher() {
+
+    }
+
+    /**
+     * Adds a new department
+     */
+    public void addDepartment() {
+
+    }
+
+    /**
+     * Displays all the students
+     */
+    public void printStudents() {
+
+    }
+
+    /**
+     * Finds student based on its id
+     * @return specified student
+     */
+    public Student findStudent() {
+
+    }
+
+    /**
+     * Adds a course
+     */
+    public void addCourse() {
+
+    }
+
+    /**
+     * Registers a student to a course
+     */
+    public void registerCourse() {
+
+    }
+
+    /**
+     * Adds a new teacher
+     */
+    public void addTeacher() {
+
+    }
+
+    /**
+     * Finds a course based on its id
+     * @return specified course
+     */
+    public Course findCourse() {
+
+    }
+
+    /**
+     * Displays all departments
+     */
+    public void findDepartments() {
+
+    }
+
+    /**
+     * Adds a new student
+     */
+    public void addStudent() {
+
+    }
+
+    /**
+     * Finds a teacher based on its id
+     * @return specified teacher
+     */
+    public Teacher findTeacher() {
+
+    }
 }
